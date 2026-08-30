992e2bbe426b0d3e00cc1c90c386b48f2d306561
Muhammad Zohaib <muhammadzohaib1550@gmail.com>
2026-08-30 11:32:09 +0500

    Time: 3 ms (27.6%), Space: 97.2 MB (87.86%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,10 +5,9 @@ public:
         int maxProfit = 0;
 
         for (int i = 1; i < prices.size(); i++) {
-            // آج کی price پر profit calculate کریں
+            
             maxProfit = max(maxProfit, prices[i] - minPrice);
 
-            // سب سے کم buying price update کریں
             minPrice = min(minPrice, prices[i]);
         }
 
